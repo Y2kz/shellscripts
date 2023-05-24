e09d59f6421703776568363c512a0340dc5ec2b4
Y2kz <39020251+Y2kz@users.noreply.github.com>
2023-05-24 14:17:36 +0530

    Create SelfExtractingScript.sh
    Create SelfExtractingScript

---
+++ SelfExtractingScript.sh
@@ -0,0 +1,16 @@
+sed '0,/^#__HERE_Y2kz_CODE_HERE__#$/d' $0|tar zx -C <__HERE_FILE_LOCATION_TO_EXTRACT__> 2>/dev/null
+#<YOUR_SCRIPT_OR_CMD_TO_EXECUTE_AFTER_UNTAR>
+sh inside_script.sh
+
+#THIS exit is mandatory as 
+exit
+#AFTER below comment do cat <__your_tar.file.tar.gz__> >> <THIS_SCRIPT_NAME.sh>
+
+#how to enbed tar to this script
+#tar -zcvf SelfExtractingScrip.tar.gz inside_script.sh
+#cat SelfExtractingScrip.tar.gz >> SelfExtractingScript.sh
+#MAKE SURE Comment "__HERE_Y2kz_CODE_HERE__" is present and there is no space in between 
+#AFTER below comment do cat <__your_tar.file.tar.gz__> >> <THIS_SCRIPT_NAME.sh>
+#__HERE_Y2kz_CODE_HERE__#
+^_<8b>^H^@<80>poE^B^CíÊA
+Â0^P<85>áY{<8a>Ð^CHCF{^D/ ë^Rj ÙÔ<92>©=¿mö.^U<94>ÿcàñf&O<96>ï©·¡äy9Ú(^_ÐnÎª{úîäknS÷<95>Wñª!^DíÚý¾^UïÅµò^EO[bqNÖ<98>m<9c>âúîïf©Ø¥Ìò_Ò0>\sM¶4^G^A^@^@^@^@^@^@^@^@^@^@^@^@^@ü<8c>^Wü<87>]^K^@(^@^@
